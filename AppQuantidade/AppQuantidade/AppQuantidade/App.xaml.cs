494874dad6c1a0f52a033ddbe3e5610281491e6e
Erick Paludo <erickpaludoyt@gmail.com>
2022-07-23 13:12:10 -0300

    1.3

---
+++ AppQuantidade/AppQuantidade/AppQuantidade/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            var imagem1 = new Image
+            {
+                Aspect = Aspect.Fill,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Fill
+            };
+            imagem1.Source = ImageSource.FromFile("background.jpg");
+
+            InitializeComponent();
+#if DEBUG
+            HotReloader.Current.Run(this);
+#endif
+
+            MainPage = new MainPage();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppQuantidade/AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace AppQuantidade
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : ContentPage
+    {
+        double média, preço, km, pedagio;
+        double resultado = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Oper
+        (object sender, double e)
+        {
+            double newText = e;
+            média = newText;
+        }
+        private void Oper_1
+        (object sender, double e)
+        {
+            double newText = e;
+            preço = newText;
+        }
+
+        private void Oper_2
+       (object sender, double e)
+        {
+            double newText = e;
+            km = newText;
+        }
+
+        private void Oper_3
+        (object sender, double e)
+        {
+            double newText = e;
+            pedagio = newText;
+        }
+
+        private void Final(object sender, EventArgs e)
+        {
+            double OperOne = km / média;
+            double OperTwo = (preço * OperOne) + pedagio;
+            resultado = OperTwo;
+            Result.Text = "R$" + resultado.ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+        private void Zerar(object sender, EventArgs e)
+        {
+            Zerarcamp_0.Text = 0.ToString();
+            Zerarcamp_1.Text = 0.ToString();
+            Zerarcamp_2.Text = 0.ToString();
+            Zerarcamp_3.Text = 0.ToString();
+            resultado = 0;
+
+
+            Result.Text = resultado.ToString();
+            
+            
+
+
+
+        }
+    }
+}
